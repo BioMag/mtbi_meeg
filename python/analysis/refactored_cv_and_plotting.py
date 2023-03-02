da432be7169200250d53da318a80c61e72f192d4
Estanislao Porta <estanislao.porta@aalto.fi>
2023-03-02 13:19:00 +0200

    Add possibility to use one segment per patient (n_segments still hardcoded). Add logging. Improve output messages

--- python/analysis/refactored_cv_and_plotting.py
+++ python/analysis/refactored_cv_and_plotting.py
@@ -19,7 +19,8 @@ from sklearn.model_selection import train_test_split, StratifiedGroupKFold, Stra
 from sklearn.svm import SVC
 
 from statistics import mean, stdev
-
+import logging
+logging.basicConfig(filename='example.log', encoding='utf-8', level=logging.DEBUG)
 
 # Pseudocode:
     # Read in dataframe and create X, y and groups
@@ -47,7 +48,7 @@ one_segment_per_subject = False
 ## Classifier
 #classifier = LinearDiscriminantAnalysis(solver='svd')
 
-# List of accuracy?
+# List containing the accuracy of the fit for each split 
 accuracies = []
 
 # Interpoling True Positive Rate
@@ -57,6 +58,9 @@ interpole_tpr = []
 tprs = []
 aucs = []
 mean_fpr = np.linspace(0, 1, 100)
+
+folds = 5
+
 #%%
 # Read in dataframe and create X, y and groups
 dataframe = pd.read_csv('dataframe.csv', index_col = 'Index')
@@ -65,79 +69,49 @@ X = dataframe.iloc[:,2:]
 y = dataframe.loc[:, 'Group']
 groups = dataframe.loc[:, 'Subject']
 
-folds = 10
 
 #%% 
 ## (Optional)
     # Define what strategy to use based on the subjects balance
 
-#%% Use all subjects and segments
-
-# # Initialize Stratified Group K Fold
-# sgkf = StratifiedGroupKFold(n_splits=folds, shuffle=True)
-# sgkf_split = sgkf.split(X, y, groups) 
-
-# Evaluate if there's any fold with only one class
-# for i, (train_index, test_index) in enumerate(sgkf_split):
-#     #print(f"Test:  index={test_index},\n{y[test_index]}")
-#     values, counts = np.unique(y[test_index], return_counts=True)
-#     if np.unique(y[test_index]).size == 1:
-#         print(f"WARN: Fold {i} has only 1 class! ####")
-#     elif verbosity == True: 
-#         fold_size = y[test_index].size
-#         if counts[0]<=counts[1]:
-#             print(f"\nFold {i}:")
-#             print(f'Class balance: {round(counts[0]/fold_size*100)}-{round(100-counts[0]/fold_size*100)}')
-#         else:
-#             print(f"\nFold {i}:")
-#             print(f'Class balance: {round(counts[1]/fold_size*100)}-{round(100-counts[1]/fold_size*100)}')
 # Note: There is a 27% chance that there's a fold with only one class. This can impact the classifier (especially LDA)
 # >>> After 999 iterations, we found 267 folds with 1 class
          
-#%% Use only one segment per subject
+#%% 
+ 
 
-# Slice data
 if one_segment_per_subject == True:
+    # TODO: Double check if choosing another segment than the 1st breaks something? It shouldnt
+    # Removes (segments-1) rows out of the dataframe X 
     X_one_segment = X[0:len(X):segments]
     y_one_segment = y[0:len(y):segments]
     groups_one_segment = groups[0:len(groups):segments]
     
     # Initialize Stratified K Fold
     skf = StratifiedKFold(n_splits=folds, shuffle=True)
-    skf_split = skf.split(X_one_segment, y_one_segment, groups_one_segment)
-    # TODO: Double check if choosing another segment than the 1st breaks something? It shouldnt
-    
-    # Iterate over the splits and check the data
-    for ii, (train_index, test_index) in enumerate(skf_split):
-        #print(f"Test:  index={test_index},\n{y_one_segment[test_index]}")
-        values, counts = np.unique(y_one_segment[test_index], return_counts=True)
-        if np.unique(y_one_segment[test_index]).size == 1:
-            print(f"WARN: Fold {ii} has only 1 class! ####")
-        elif verbosity == True: 
-            fold_size_one_segment = y_one_segment[test_index].size
-            print(f"\nFold {ii}:")
-            if counts[0]<=counts[1]:
-                print(f'Class balance: {round(counts[0]/fold_size_one_segment*100)}-{round(100-counts[0]/fold_size_one_segment*100)}')
-            else:
-                print(f'Class balance: {round(counts[1]/fold_size_one_segment*100)}-{round(100-counts[1]/fold_size_one_segment*100)}')    
-
-#%%
-
-
-# Initialize Stratified Group K Fold
-sgkf = StratifiedGroupKFold(n_splits=folds, shuffle=True)
-sgkf_split = sgkf.split(X, y, groups) 
+    data_split = skf.split(X_one_segment, y_one_segment, groups_one_segment)
+else:
+    # Initialize Stratified Group K Fold
+    sgkf = StratifiedGroupKFold(n_splits=folds, shuffle=True)
+    data_split = sgkf.split(X, y, groups)
 
 # Initialize figure for plottting
 fig, ax = plt.subplots(figsize=(6, 6))
 
 # Define classifier
 classifier = SVC(probability=True)
-for split, (train_index, test_index) in enumerate(sgkf_split):
+for split, (train_index, test_index) in enumerate(data_split):
     # Generate train and test sets for this split
-    X_train, X_test = X.iloc[train_index], X.iloc[test_index]
-    y_train, y_test = y.iloc[train_index], y.iloc[test_index]
-    
+    if one_segment_per_subject == True:
+        X_train, X_test = X_one_segment.iloc[train_index], X_one_segment.iloc[test_index]
+        y_train, y_test = y_one_segment.iloc[train_index], y_one_segment.iloc[test_index]
+    else:
+        X_train, X_test = X.iloc[train_index], X.iloc[test_index]
+        y_train, y_test = y.iloc[train_index], y.iloc[test_index]
+     
+    logging.info(f'Shape of X_train is {X_train.shape[0]} x {X_train.shape[1]}')
+    logging.info(f'Shape of X_test is {X_test.shape[0]} x {X_test.shape[1]}')
+
     # Fit classifier
     classifier.fit(X_train, y_train)
     
@@ -157,19 +131,18 @@ for split, (train_index, test_index) in enumerate(sgkf_split):
     y_pred = classifier.predict(X_test).astype(int)
     # Estimate accuracy for this split (normalized), and append to list of accuracies
     accuracies.append(accuracy_score(y_test, y_pred))    
-    
     # interpolate to build a series of 'y' values that correspond to linspace 'mean_fpr' 
     interpole_tpr = np.interp(mean_fpr, viz.fpr, viz.tpr)
     # Adds intercept just in case I guess
     interpole_tpr[0] = 0.0
-    
+    logging.info(f'AUC for split {split} = {viz.roc_auc}\n')
     tprs.append(interpole_tpr) 
     aucs.append(viz.roc_auc)
-
+    
     # Control if there's only one class in a fold
     values, counts = np.unique(y[test_index], return_counts=True)
     if np.unique(y[test_index]).size == 1:
-        print(f"WARN: Fold {split} has only 1 class! ####")
+        logging.warn(f"WARN: Fold {split} has only 1 class! ####")
     elif verbosity == True: 
         fold_size = y[test_index].size
         if counts[0]<=counts[1]:
@@ -193,13 +166,15 @@ ax.plot(
     color="b",
     label=r"Mean ROC (AUC = %0.2f $\pm$ %0.2f)" % (mean_auc, std_auc),
     lw=2,
-    alpha=0.8,
+    alpha=0.8
 )
 
-average_accuracy = mean(accuracies)
-accuracy_std = stdev(accuracies)
-AUC = (round(mean(aucs),3), round(stdev(aucs), 3))
-print(f"Average accuracy: {round(average_accuracy,3)}, Standard deviation of accuracies: {round(accuracy_std,3)}\nAUC = {AUC}")
+# Calculate the mean again
+accuracy_average = round(mean(accuracies), 3)
+accuracy_std = round(stdev(accuracies), 3)
+AUC_mean = round(mean(aucs), 3)
+AUC_std = round(stdev(aucs), 3)
+print(f"Mean accuracy: {accuracy_average} ± {accuracy_std}\nAUC = {AUC_mean} ± {AUC_std}")
 
 std_tpr = np.std(tprs, axis=0)
 tprs_upper = np.minimum(mean_tpr + std_tpr, 1)
