08114ab69ba42ca0bce8b8187c3297070fc6f96b
Estanislao Porta <estanislao.porta@aalto.fi>
2023-04-05 11:48:44 +0300

    Add setup.py and requirements for packaging.

---
+++ setup.py
@@ -0,0 +1,22 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name='mtbi_meeg',
+    version='0.1.0',
+    description='Pipeline to analyze EEG and MEG data and determine minor Trauma Brain Injuries',
+    author=['Verna Heikkinen', 'Estanislao Porta', 'Aino Kuusi'],
+    author_email=['verna.heikkinen@example.com', 'estanislao.porta@aalto.fi'],
+    install_requires=requirements,
+    url='githubURL',
+    package_dir = {'': 'src'},
+#    packages=find_packages(),
+    packages = ['analysis', 'processing'],
+    classifiers=[    
+	'Programming Language :: Python :: 3',
+	'License :: OSI Approved :: MIT License',
+	'Operating System :: OS Independent',
+	],
+)
